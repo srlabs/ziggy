5dc61c0d73c1fa16c76a670b5ab81da57a6b8c8a
Louis Merlin <louis@srlabs.de>
2023-06-13 17:41:05 +0200

    Fix clippy warning

--- examples/url/src/main.rs
+++ examples/url/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     ziggy::fuzz!(|data: &[u8]| {
         if let Ok(s) = std::str::from_utf8(data) {
-            if let Ok(_parsed) = url::Url::parse(&s) {
+            if let Ok(_parsed) = url::Url::parse(s) {
                 #[cfg(not(fuzzing))]
                 println!("{_parsed}");
             }
