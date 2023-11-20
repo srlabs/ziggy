5960cc658113522436114c48839b77c669b961d1
Louis Merlin <louis@srlabs.de>
2023-11-20 14:47:42 +0100

    Require cargo-afl version for add-seeds, and update some dependencies

--- src/bin/cargo-ziggy/add_seeds.rs
+++ src/bin/cargo-ziggy/add_seeds.rs
@@ -5,6 +5,25 @@ impl AddSeeds {
     pub fn add_seeds(&mut self) -> Result<(), anyhow::Error> {
         eprintln!("Adding seeds to AFL");
 
+        let req = semver::VersionReq::parse(">=0.14.5").unwrap();
+        let cargo = env::var("CARGO").unwrap_or_else(|_| String::from("cargo"));
+        let afl_version = process::Command::new(cargo)
+            .args(["afl", "--version"])
+            .output()
+            .context("could not run `cargo afl --version`")?;
+
+        if !std::str::from_utf8(afl_version.stdout.as_slice())
+            .unwrap_or_default()
+            .split_whitespace()
+            .nth(1)
+            .context("could not get afl version from stdout")
+            .map(semver::Version::parse)
+            .context("could not parse cargo-afl version")?
+            .map(|v| req.matches(&v))?
+        {
+            return Err(anyhow!("Outdated version of cargo-afl, ziggy needs >=0.14.5, please run `cargo install cargo-afl`"));
+        }
+
         self.target = find_target(&self.target)?;
 
         let input = self
