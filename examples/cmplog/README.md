d760e844bb03dbcba07cc88c6f12217f8aa8450a
Louis Merlin <louis@srlabs.de>
2023-11-20 14:19:17 +0100

    Add first version of cmplog testing

---
+++ examples/cmplog/README.md
@@ -0,0 +1,13 @@
+# Ziggy example - CMPLOG
+
+First, install the tooling:
+
+```
+cargo install cargo-afl honggfuzz ziggy
+```
+
+Then, in this directory, run:
+
+```
+cargo ziggy fuzz
+```
---
+++ examples/cmplog/src/main.rs
@@ -0,0 +1,38 @@
+fn main() {
+    // This fuzz harness demonstrates the capabilities of CmpLog.
+    // Simply run the fuzzer and it should find the crash immediately.
+    ziggy::fuzz!(|data: &[u8]| {
+        if data.len() < 29 {
+            return;
+        }
+        if data[0] != b'A' {
+            return;
+        }
+        if data[1] != b'B' {
+            return;
+        }
+        if data[2] != b'C' {
+            return;
+        }
+        if data[3] != b'D' {
+            return;
+        }
+
+        if data[4..8] != 0x6969_4141_i32.to_le_bytes() {
+            return;
+        };
+
+        if data[8..12] != *b"1234" || data[12..16] != *b"EFGH" {
+            return;
+        };
+
+        let slice = &data[16..];
+        let match_string = "Hello, world!";
+        let compare_string = String::from_utf8(slice.to_vec()).unwrap_or_default();
+        if compare_string != match_string {
+            return;
+        }
+
+        panic!("BOOM");
+    });
+}
